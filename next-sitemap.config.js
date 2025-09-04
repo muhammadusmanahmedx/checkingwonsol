03b1282b1ea7d18644b928a9f597b33ea2772cc7
Muhammad Usman Ahmed <usmanahmed3377@gmail.com>
2025-09-04 16:50:32 +0500

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://wonsol.com',
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: 'monthly', // realistic update frequency
+  priority: 0.7,
+};
